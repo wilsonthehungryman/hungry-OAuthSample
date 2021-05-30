6e13d9b331b9f60a4edd6a87228443f4f07fe1a4
wilson.reid <wilsonthehungryman@gmail.com>
2021-05-30 18:51:44 -0500

    Add endpoint, serialization works

--- src/infrastructure/routing.OAuthRoutes.kt
+++ src/infrastructure/routing.OAuthRoutes.kt
@@ -1,6 +1,11 @@
 package com.hungry.oauthsample.infrastructure
 
 import com.hungry.oauthsample.api.OAuthApi
+import com.hungry.oauthsample.api.dto.`in`.CreateUserDto
+import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.receive
+import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.post
 import io.ktor.routing.route
@@ -8,7 +13,8 @@ import io.ktor.routing.route
 fun Route.oauthRoutes(oAuthApi: OAuthApi) {
     route("/user") {
         post {
-
+            oAuthApi.createUser(call.receive<CreateUserDto>())
+            call.respond(HttpStatusCode.Created, "User Created")
         }
     }
 }
