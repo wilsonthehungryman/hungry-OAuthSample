75c5c4898ee72e3853890a5603574a04c929388d
wilson.reid <wilsonthehungryman@gmail.com>
2021-05-30 20:33:26 -0500

    Actually hashing pwds, needs a review

--- src/domain/PasswordService.kt
+++ src/domain/PasswordService.kt
@@ -1,13 +1,23 @@
 package com.hungry.oauthsample.domain
 
-import java.util.UUID
+import org.mindrot.jbcrypt.BCrypt
 
 class PasswordService {
-    val pepper = "replace me"
+    // TODO shouldn't be hardcoded, and should be secret and unique per env
+    private val pepper = "36f61d32-6c9c-4641-9f32-9a3fce8f1b8f"
 
-    fun hashPassword(password: String): Pair<String, String> {
-        val salt = UUID.randomUUID().toString()
-        // TODO actually hash
-        return Pair("todo", salt)
+    fun hashPassword(password: String): String {
+        val passwordWithPepper = BCrypt.hashpw(password, pepper)
+        return BCrypt.hashpw(passwordWithPepper, BCrypt.gensalt())
+    }
+
+    fun isMatchingPassword(password: String, hashedPassword: String): Boolean {
+        val passwordWithPepper = BCrypt.hashpw(password, pepper)
+        return BCrypt.checkpw(passwordWithPepper, hashedPassword)
+    }
+
+    fun isPreviouslyUsed(password: String, hashedPasswords: Set<String>): Boolean {
+        val passwordWithPepper = BCrypt.hashpw(password, pepper)
+        return hashedPasswords.stream().anyMatch { BCrypt.checkpw(passwordWithPepper, it) }
     }
 }
