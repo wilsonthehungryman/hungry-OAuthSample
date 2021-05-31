5554d75666b987262457ad2fb51bbcc195e2d9a3
wilson.reid <wilsonthehungryman@gmail.com>
2021-05-30 19:03:28 -0500

    User entity/table

--- src/infrastructure/SqlUserRepository.kt
+++ src/infrastructure/SqlUserRepository.kt
@@ -2,10 +2,21 @@ package com.hungry.oauthsample.infrastructure
 
 import com.hungry.oauthsample.com.hungry.oauthsample.domain.CreateUser.kt.CreateUser
 import com.hungry.oauthsample.domain.UserRepository
-import com.hungry.oauthsample.infrastructure.config.AppConfig
+import org.jetbrains.exposed.dao.id.LongIdTable
+import org.jetbrains.exposed.sql.Column
+
+internal object User: LongIdTable() {
+    val email: Column<String> = varchar("email", 200)
+    val password: Column<String> = varchar("password", 200)
+    val phoneNumber: Column<String> = varchar("phoneNumber", 100)
+    val name: Column<String?> = varchar("name", 200).nullable()
+    val age: Column<Int?> = integer("age").nullable()
+}
 
 class SqlUserRepository: UserRepository {
     override fun save(user: CreateUser, hashedPassword: String, salt: String) {
         TODO("Not yet implemented")
     }
+
+
 }
