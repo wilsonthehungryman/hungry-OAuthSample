4292b8df635bb2beac2d36129d9e3e568faeba83
wilson.reid <wilsonthehungryman@gmail.com>
2021-06-05 16:49:46 -0500

    Update koin module

--- src/infrastructure/KoinModule.kt
+++ src/infrastructure/KoinModule.kt
@@ -4,12 +4,18 @@ import com.hungry.oauthsample.api.OAuthApi
 import com.hungry.oauthsample.domain.OAuthService
 import com.hungry.oauthsample.domain.PasswordService
 import com.hungry.oauthsample.domain.UserRepository
+import com.hungry.oauthsample.domain.tokens.TokenRepository
+import com.hungry.oauthsample.domain.tokens.TokenService
 import org.koin.dsl.module
 
 
 val userKoinModule = module(createdAtStart = true) {
     single { SqlUserRepository() as UserRepository }
+    single { SqlTokenRepository() as TokenRepository }
+    
     single { PasswordService() }
+    single { TokenService(get()) }
     single { OAuthService(get(), get()) }
+
     single { OAuthApi(get()) }
 }
