9526386ec36986c1c357355809c92f377cf93269
wilson.reid <wilsonthehungryman@gmail.com>
2021-06-16 18:54:09 -0500

    Update koin bindings

--- src/infrastructure/KoinModule.kt
+++ src/infrastructure/KoinModule.kt
@@ -6,11 +6,13 @@ import com.hungry.oauthsample.domain.oauth.PasswordService
 import com.hungry.oauthsample.domain.users.UserRepository
 import com.hungry.oauthsample.domain.client.ClientRepository
 import com.hungry.oauthsample.domain.oauth.CodeRepository
+import com.hungry.oauthsample.domain.tokens.RevokedTokenCache
 import com.hungry.oauthsample.domain.tokens.TokenRepository
 import com.hungry.oauthsample.domain.tokens.TokenService
 import com.hungry.oauthsample.infrastructure.config.AppConfig
 import com.hungry.oauthsample.infrastructure.sql.SqlClientRepository
 import com.hungry.oauthsample.infrastructure.sql.SqlCodeRepository
+import com.hungry.oauthsample.infrastructure.sql.SqlRevokedTokenCache
 import com.hungry.oauthsample.infrastructure.sql.SqlTokenRepository
 import com.hungry.oauthsample.infrastructure.sql.SqlUserRepository
 import org.koin.dsl.module
@@ -19,11 +21,12 @@ import org.koin.dsl.module
 val userKoinModule = module(createdAtStart = true) {
     single { SqlUserRepository() as UserRepository }
     single { SqlTokenRepository() as TokenRepository }
+    single { SqlRevokedTokenCache() as RevokedTokenCache }
     single { SqlClientRepository() as ClientRepository }
     single { SqlCodeRepository() as CodeRepository }
     
     single { PasswordService() }
-    single { TokenService(get(), get<AppConfig>().keysConfig) }
+    single { TokenService(get(), get(), get<AppConfig>().keysConfig) }
     single { OAuthService(get(), get(), get(), get(), get()) }
 
     single { OAuthApi(get()) }
