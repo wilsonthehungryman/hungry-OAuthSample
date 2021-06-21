8db6f42b19cc08a6a21e4c361d01b279f6ddcfdd
wilson.reid <wilsonthehungryman@gmail.com>
2021-06-21 18:46:18 -0500

    Map 401s 403s and some 400s scenarios, also cleaned up pom

--- src/Application.kt
+++ src/Application.kt
@@ -1,15 +1,22 @@
 package com.hungry.oauthsample
 
+import com.fasterxml.jackson.core.JsonParseException
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.hungry.oauthsample.api.OAuthApi
+import com.hungry.oauthsample.domain.Forbidden
+import com.hungry.oauthsample.domain.Unauthorized
 import com.hungry.oauthsample.infrastructure.config.AppConfig
 import com.hungry.oauthsample.infrastructure.oauthRoutes
 import com.hungry.oauthsample.infrastructure.userKoinModule
 import io.ktor.application.Application
+import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.auth.Authentication
 import io.ktor.features.ContentNegotiation
+import io.ktor.features.StatusPages
+import io.ktor.http.HttpStatusCode
 import io.ktor.jackson.jackson
+import io.ktor.response.respond
 import io.ktor.routing.routing
 import io.ktor.util.KtorExperimentalAPI
 import org.jetbrains.exposed.sql.Database
@@ -27,13 +34,16 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module(testing: Boolean = false) {
     val appConfig = AppConfig()
 
-    install(Authentication) {
-    }
-
     install(ContentNegotiation) {
         jackson {
             enable(SerializationFeature.INDENT_OUTPUT)
         }
+    }
+
+    install(StatusPages) {
+        exception<Unauthorized> { this.context.respond(HttpStatusCode.Unauthorized) }
+        exception<Forbidden> { this.context.respond(HttpStatusCode.Forbidden) }
+        exception<JsonParseException> { this.context.respond(HttpStatusCode.BadRequest) }
     }
 
     val db = appConfig.sqlConfig.dataSource
